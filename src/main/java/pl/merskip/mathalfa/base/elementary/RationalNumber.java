2dcbf710b84fe486f95c2b997fbee5b112485675
Piotr Merski <merskip@gmail.com>
2016-12-31 15:50:22 +0100

    Dodanie możliwości uproszczenia liczb

--- src/main/java/pl/merskip/mathalfa/base/elementary/RationalNumber.java
+++ src/main/java/pl/merskip/mathalfa/base/elementary/RationalNumber.java
@@ -5,6 +5,8 @@ import pl.merskip.mathalfa.base.core.Operation;
 import pl.merskip.mathalfa.base.core.Symbol;
 import pl.merskip.mathalfa.base.operation.CalculateOperation.Calculable;
 
+import static java.lang.Math.abs;
+
 public class RationalNumber implements Number, Calculable {
 
     private int numerator;
@@ -30,6 +32,30 @@ public class RationalNumber implements Number, Calculable {
     @Override
     public Symbol calculate(Operation operation) {
         return this;
+    }
+    
+    public RationalNumber simplify() {
+        int sign = numerator * denominator >= 0 ? 1 : -1;
+        int gcd = getGCD(abs(numerator), abs(denominator));
+        if (gcd > 1) {
+            return new RationalNumber(abs(numerator) / gcd * sign, abs(denominator) / gcd);
+        }
+        else {
+            int numerator = abs(this.numerator) * sign;
+            int denominator = abs(this.denominator);
+            if (numerator == this.numerator && denominator == this.denominator) {
+                return this;
+            }
+            else {
+                return new RationalNumber(numerator, denominator);
+            }
+        }
+    }
+    
+    private int getGCD(int a, int b) {
+        if (b == 0)
+            return a;
+        return getGCD(b, a % b);
     }
     
     public String toPlainText() {
